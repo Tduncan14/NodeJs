c9ef9a5d5275d3ed79cf9aa1ac8afc3ee8561302
unknown <nbatre1@yahoo.com>
2019-02-08 16:05:44 -0600

    keep moving forward

--- index.js
+++ index.js
@@ -13,8 +13,71 @@ const server = http.createServer((req,res) =>{
 
       })
 
-  }
+
+  /*
+
+  if(req.url === '/api/users'){
+    const users = [
+        {name:"treek"},
+        { age: '24'},
+        {height:73}
+    ];
+    res.writeHead(200,{'Content-Type':'applications/json'});
+    res.end(JSON.stringify(users));
+
+     
+};
+
 });
+*/ // Bui;d file path
+let filepath =  path.join(__dirname,'public', req.url === '/' ? 'index.html':req.url);
+
+let extname= path.extname(filepath);
+
+// intial content-tyle
+let contentType = 'text/html';
+
+// check ext and set content type
+switch(extname){
+    case '.js':
+    contentType ='text/javascript'
+    break;
+    case '.css':
+    contentType = 'text/css'
+    break;
+    case '.json':
+    contentType = 'application/json'
+    break;
+
+
+}
+
+//readFile
+fs.readFile(filepath,(err,content) =>{
+    if(err){
+        if(err.code == 'ENOENT'){
+            //Page not found
+            fs.readFile(path.join(__dirname,'public','404.html'),(err,content) =>{
+             res.writeHead(200,{'Content-Type':'text/html'});
+             res.end(content,'utf-8');
+            })
+    }
+    else{
+        // a server error
+        res.writeHead(500);
+        res.end(`Server error : ${err.code}`);
+    }
+}
+ else{
+     // success
+     res.writeHead(200,{'Content-Type': contentType});
+     res.end(content,'utf-8');
+ }
+
+})
+
+}})
+
 
 const PORT = process.env.PORT ||5000;
 
